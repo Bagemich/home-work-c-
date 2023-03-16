b50b84b5ce4c248e1bf95d3c27dd255229aa2b01
znako <127133685+Bagemich@users.noreply.github.com>
2023-03-16 22:48:12 +0500

    Add files via upload

---
+++ 2Program.cs
@@ -0,0 +1,14 @@
+﻿int Prompt(string msg)
+{
+    Console.WriteLine($"[smg]");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int number = Prompt("Введите трёхзначное число");
+if (number > 100 && number < 1000)
+{
+    int mid = (number % 100 - number % 10) / 10;
+    Console.WriteLine($"Вторая цифра {number} получается {mid}");
+}
+
+else
+Console.WriteLine("это фигня");
