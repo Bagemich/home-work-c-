6c22a44183fd26cc4c670367669a481be28a59b4
znako <127133685+Bagemich@users.noreply.github.com>
2023-03-16 22:54:02 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿int Prompt(string msg)
+{
+Console.WriteLine($"{msg}");
+return Convert.ToInt32(Console.ReadLine());
+}
+int number = Prompt("Введите число");
+string ss = ("Это не день недели");
+string check(int number)
+{
+if (number >= 6 && number < 9) ss = "Это выходной";
+if (number >= 1 && number < 6) ss = "Это будни";
+return ss;
+}
+Console.WriteLine(check(number));
+
